0e848b4332a4aa933dc5d8cfaafc6e612ab5e431
abobkar <abobakryousre@gmail.com>
2024-08-09 17:59:04 +0300

    adding basic HTML

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
 echo "hello from php.." . PHP_EOL;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+
+<body>
+    <h1>Index</h1>
+</body>
+
+</html>
